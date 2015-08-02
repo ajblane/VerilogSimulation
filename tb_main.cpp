8e69dbc3e1e3dc63460a6e1b2e0f2c8b647cc5f6
AJ <ajblane0612@gmail.com>
2015-08-02 09:29:13 +0800

    implement clock cycle for simulation

--- tb_main.cpp
+++ tb_main.cpp
@@ -2,24 +2,40 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
+
+#define CYCLE() do { \
+		top->clk = 0; \
+		top->eval(); \
+		if (tfp) tfp->dump(eval_num ++); \
+		top->clk = 1; \
+		top->eval(); \
+		if (tfp) tfp->dump(eval_num ++); \
+		}while (0)
+
 int main(int argc, char **argv, char **env)
 {
-    int clk;
+    int eval_num = 0;
+    int i = 0;
     Verilated::commandArgs(argc, argv);
     Vdemo_axi_memory *top = new Vdemo_axi_memory;
     Verilated::traceEverOn(true);
     VerilatedVcdC *tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
     tfp->open("trace.vcd");
-    top->clk = 0;
-    for(clk = 0; clk <= 4; clk++)
-    {
-        tfp->dump(clk);
-        top->clk = !top->clk;
-        top->eval();
-    }
-    tfp->close();
-    exit(0);
 
-  return 0;
+   
+    top->reset_n = 0;   
+    for(i = 0 ; i < 10 ; i++)
+    { 
+	CYCLE();
+    }	
+    top->reset_n = 1;
+    CYCLE();
+
+ 
+    if (tfp)
+        tfp->close();
+    
+    delete tfp;
+    return 0;
 }
